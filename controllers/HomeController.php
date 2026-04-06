28563947b31b43fdc4c76c9691680f9210ac07c4
datmega4-bit <datmega4@gmail.com>
2026-04-07 04:46:20 +0700

    done

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-class HomeController
-{
-    public function index()
-    {
-        require_once PATH_VIEW . 'main.php';
-    }
-    public function about()
-    {
-        require_once PATH_VIEW . 'about.php';
-    }
-    public function contact()
-    {
-        require_once PATH_VIEW . 'contact.php';
-    }
-    public function services()
-    {
-        require_once PATH_VIEW . 'services.php';
-    }
-    public function blog()
-    {
-        require_once PATH_VIEW . 'blog.php';
-    }
-}
-=======
 <?php
 
 class HomeController
@@ -38,4 +11,3 @@ class HomeController
         require_once PATH_VIEW . 'about.php';
     }
 }
->>>>>>> 3801a107b199a547d599a7e4aa9e07da6c5f5fb3
--- routes/index.php
+++ routes/index.php
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 <?php
 
 $action = $_GET['action'] ?? '/';
 
 match ($action) {
     '/' => (new HomeController)->index(),
-=======
-<?php
-
-$action = $_GET['action'] ?? '/';
-
-match ($action) {
-    '/'         => (new HomeController)->index(),
->>>>>>> 3801a107b199a547d599a7e4aa9e07da6c5f5fb3
 };
