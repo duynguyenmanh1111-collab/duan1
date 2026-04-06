10a4272be236e198de2c4f9023a3d808ea355bc4
datmega4-bit <datmega4@gmail.com>
2026-04-07 04:17:41 +0700

    helo

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -6,4 +6,20 @@ class HomeController
     {
         require_once PATH_VIEW . 'main.php';
     }
+    public function about()
+    {
+        require_once PATH_VIEW . 'about.php';
+    }
+    public function contact()
+    {
+        require_once PATH_VIEW . 'contact.php';
+    }
+    public function services()
+    {
+        require_once PATH_VIEW . 'services.php';
+    }
+    public function blog()
+    {
+        require_once PATH_VIEW . 'blog.php';
+    }
 }
